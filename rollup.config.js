ddbf85e89701c4c1b25cd8d15e6719f665489c28
Clément Léger <clement.leger@kleegroup.com>
2019-09-11 15:28:28 +0200

    Auto compile dev env plus webserver

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,19 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
+let plugins = [
+    commonjs(),
+    resolve(),
+    babel(),
+];
+if(process.env.BUILD === 'release')
+    plugins.append(terser());
+
 export default {
     input: 'src/index.js',
     output: {
         format: 'es',
         file: 'lib/frame.es.min.js'
     },
-    plugins: [
-        commonjs(),
-        resolve(),
-        babel(),
-        terser()
-    ]
+    plugins,
 };
