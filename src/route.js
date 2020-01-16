cce8bba2cebed9f55daabd326c60f54187f7d151
Clément Léger <clement.leger@kleegroup.com>
2020-01-16 16:35:55 +0100

    support landing on any route

--- src/route.js
+++ src/route.js
@@ -3,11 +3,15 @@ import {mount} from './mount';
 export function route(root, table, base) {
     route.table = table;
     route.root = root;
-    route.base = typeof(base) === 'string' ? base : document.location.pathname + '#!';
+    route.base = typeof(base) === 'string' ? base : document.location.pathname + '#';
 
     window.addEventListener('popstate', (event) => {
         route.set(document.location.pathname, event.state);
     });
+
+    console.log('router base : ' + document.location.pathname)
+    route.set(document.location.pathname + document.location.hash);
+
     return route;
 }
 
@@ -16,7 +20,10 @@ route.table = null;
 route.set = function(path, state) {
     let matchingRoute = route.get(path);
     if(matchingRoute) {
-        mount(route.root, route.table[path]);
+        mount(route.root, matchingRoute);
+    }
+    else {
+        console.error('No route found ' + path);
     }
 };
 
@@ -26,6 +33,13 @@ route.go = function(path, state) {
 };
 
 route.get = function(path) {
+    if(route.base.substr(-1) === '#' && path + '#' === route.base) {
+        path = '/';
+    }
+    if(path.startsWith(route.base)) {
+        path = path.substring(route.base.length);
+    }
+    console.log('get ' + path)
     if(path in route.table) {
         return route.table[path];
     }
